269d61fe0a991d77a2f3056bded8ccfffef731a6
HeymerDev <heymermeza11@gmail.com>
2026-02-24 23:42:09 -0500

    create Budget Model

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,12 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 
+import path from "path";
+
 dotenv.config();
 
 export const db = new Sequelize(process.env.DATABASE_URL, {
+  models: [path.join(__dirname, "..", "models")],
   dialectOptions: {
     ssl: {
       require: false,
