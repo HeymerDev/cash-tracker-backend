e70fefce6277b8fd354d3004d8e794e5da68c88d
HeymerDev <heymermeza11@gmail.com>
2026-02-24 23:13:51 -0500

    connect to BD on neon

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,20 @@ import express from "express";
 import colors from "colors";
 import morgan from "morgan";
 
+import { db } from "./config/db";
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold("Database connected successfully"));
+  } catch (error) {
+    console.log(colors.red.bold("Error connecting to database:" + error));
+  }
+}
+
+connectDB();
+
 const app = express();
 
 app.use(morgan("dev"));
