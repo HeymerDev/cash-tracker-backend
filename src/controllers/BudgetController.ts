83771269f618cb7b67a0af29036b3ee0798e37c8
HeymerDev <heymermeza11@gmail.com>
2026-02-27 00:03:27 -0500

    add controller & routes for all budget method

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -4,4 +4,23 @@ export class BudgetController {
   static getAll = async (req: Request, res: Response) => {
     res.status(200).json({ message: "Get all budget entries" });
   };
+
+  static create = async (req: Request, res: Response) => {
+    res.status(201).json({ message: "Create a new budget entry" });
+  };
+
+  static getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    res.status(200).json({ message: `Get budget entry with ID: ${id}` });
+  };
+
+  static update = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    res.status(200).json({ message: `Update budget entry with ID: ${id}` });
+  };
+
+  static delete = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    res.status(200).json({ message: `Delete budget entry with ID: ${id}` });
+  };
 }
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -4,5 +4,9 @@ import { BudgetController } from "../controllers/BudgetController";
 const router: Router = Router();
 
 router.get("/", BudgetController.getAll);
+router.post("/", BudgetController.create);
+router.get("/:id", BudgetController.getById);
+router.patch("/:id", BudgetController.update);
+router.delete("/:id", BudgetController.delete);
 
 export default router;
