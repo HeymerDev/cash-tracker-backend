65590d3de5c00bc6cc330f4387177a9a63579597
HeymerDev <heymermeza11@gmail.com>
2026-03-05 23:02:44 -0500

    add validation on create budget & create validation middleware

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,10 +1,25 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { BudgetController } from "../controllers/BudgetController";
+import { handleInputErrors } from "../middlewares/validation";
 
 const router: Router = Router();
 
 router.get("/", BudgetController.getAll);
-router.post("/", BudgetController.create);
+router.post(
+  "/",
+  body("name").notEmpty().withMessage("Name is required"),
+  body("amount")
+    .notEmpty()
+    .withMessage("Amount is required")
+    .isNumeric()
+    .withMessage("Amount must be a number")
+    .isFloat({ gt: 0 })
+    .withMessage("Amount must be a positive number"),
+
+  handleInputErrors,
+  BudgetController.create,
+);
 router.get("/:id", BudgetController.getById);
 router.patch("/:id", BudgetController.update);
 router.delete("/:id", BudgetController.delete);
