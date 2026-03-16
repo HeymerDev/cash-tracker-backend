056ee4e9b1a0e740c0303fa128302256d666eade
HeymerDev <heymermeza11@gmail.com>
2026-03-16 16:56:11 -0500

    add update expense controller

--- src/controllers/ExpenseController.ts
+++ src/controllers/ExpenseController.ts
@@ -24,7 +24,11 @@ export class ExpenseController {
     res.status(200).json(expense);
   };
 
-  static update = async (req: Request, res: Response) => {};
+  static update = async (req: Request, res: Response) => {
+    const { expense } = req;
+    await expense.update(req.body);
+    res.status(200).json({ message: "Expense entry updated successfully" });
+  };
 
   static delete = async (req: Request, res: Response) => {};
 }
