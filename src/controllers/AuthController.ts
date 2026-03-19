696b026dc701f13679283220d45641c313be2d04
HeymerDev <heymermeza11@gmail.com>
2026-03-18 22:41:39 -0500

    add register authController

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,7 +3,20 @@ import User from "../models/User";
 
 export class AuthController {
   static register = async (req: Request, res: Response) => {
-    console.log("Desde register");
+    try {
+      const userExists = await User.findOne({
+        where: { email: req.body.email },
+      });
+      if (userExists) {
+        return res.status(400).json({ message: "Email already in use" });
+      }
+      const user = new User(req.body);
+      await user.save();
+      res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+      //console.log(error);
+      res.status(500).json({ message: "Error registering user", error });
+    }
   };
 
   static login = async (req: Request, res: Response) => {};
