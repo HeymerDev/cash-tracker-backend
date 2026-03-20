96658203abe8a1c455ad4f1f6f057c92901469dd
HeymerDev <heymermeza11@gmail.com>
2026-03-19 21:16:26 -0500

    add password hashed

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,16 +1,21 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../helpers/auth";
 
 export class AuthController {
   static register = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const userExists = await User.findOne({
+      where: { email },
+    });
+
     try {
-      const userExists = await User.findOne({
-        where: { email: req.body.email },
-      });
       if (userExists) {
-        return res.status(400).json({ message: "Email already in use" });
+        return res.status(409).json({ message: "Email already in use" });
       }
+
       const user = new User(req.body);
+      user.password = await hashPassword(password);
       await user.save();
       res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
