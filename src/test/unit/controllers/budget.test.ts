3a82873d4d32a63f8608ec5ae69e2f8cf14283be
HeymerDev <heymermeza11@gmail.com>
2026-04-07 23:23:55 -0500

    add test for should return not all budgets for a user & should return all budgets for a user by ID on budgetController

--- src/test/unit/controllers/budget.test.ts
+++ src/test/unit/controllers/budget.test.ts
@@ -1,7 +1,56 @@
+import { createResponse, createRequest } from "node-mocks-http";
 import { budgets } from "../../mocks/controllers/budget";
+import { BudgetController } from "../../../controllers/BudgetController";
+import Budget from "../../../models/Budget";
+
+jest.mock("../../../models/Budget.ts", () => ({
+  findAll: jest.fn(),
+}));
 
 describe("BudgetController.getAll", () => {
   test("should return all budgets for a user", async () => {
-    expect(budgets).toHaveLength(3);
+    const req = createRequest({
+      method: "GET",
+      url: "/api/budgets",
+      user: { id: 1 },
+    });
+
+    const res = createResponse();
+
+    const filteredBudgets = budgets.filter((b) => b.userId === req.user.id);
+
+    (Budget.findAll as jest.Mock).mockResolvedValue(filteredBudgets);
+    await BudgetController.getAll(req, res);
+
+    const data = res._getJSONData();
+
+    console.log(data);
+
+    expect(res.statusCode).toBe(200);
+    expect(filteredBudgets).toHaveLength(2);
+    expect(data).toEqual(filteredBudgets);
+  });
+
+  test("should return not all budgets for a user", async () => {
+    const req = createRequest({
+      method: "GET",
+      url: "/api/budgets",
+      user: { id: 10 },
+    });
+
+    const res = createResponse();
+
+    const filteredBudgets = budgets.filter((b) => b.userId === req.user.id);
+
+    (Budget.findAll as jest.Mock).mockResolvedValue(filteredBudgets);
+    await BudgetController.getAll(req, res);
+
+    const data = res._getJSONData();
+
+    console.log(data);
+
+    expect(res.statusCode).toBe(200);
+    expect(filteredBudgets).toHaveLength(0);
+    expect(data).toEqual(filteredBudgets);
   });
 });
