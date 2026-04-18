adb3d8b8a6aca714128e3c1ad44a4569a238fad9
HeymerDev <heymermeza11@gmail.com>
2026-04-17 21:43:49 -0500

    add testing on middleware validateBudgetExists if budget entry exist called next function and budget travel on req.budget

--- src/test/unit/middlewares/budget.test.ts
+++ src/test/unit/middlewares/budget.test.ts
@@ -1,6 +1,7 @@
 import { createResponse, createRequest } from "node-mocks-http";
 import { validateBudgetExists } from "../../../middlewares/budget";
 import Budget from "../../../models/Budget";
+import { budgets } from "../../mocks/controllers/budget";
 
 jest.mock("../../../models/Budget.ts", () => ({
   findByPk: jest.fn(),
@@ -22,5 +23,19 @@ describe("validateBudgetExists", () => {
     const data = res._getJSONData();
     expect(data).toEqual({ message: "Budget entry not found" });
     expect(next).not.toHaveBeenCalled();
+  });
+
+  test("should attach budget to req if it exists", async () => {
+    (Budget.findByPk as jest.Mock).mockResolvedValue(budgets[0]);
+    const req = createRequest({
+      params: { budgetId: "1" },
+    });
+    const res = createResponse();
+    const next = jest.fn();
+
+    await validateBudgetExists(req, res, next);
+
+    expect(req.budget).toEqual(budgets[0]);
+    expect(next).toHaveBeenCalled();
   });
 });
