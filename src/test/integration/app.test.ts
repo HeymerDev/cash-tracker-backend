59d8550edff79c6d6c7553b1c762611a982bca36
HeymerDev <heymermeza11@gmail.com>
2026-05-07 21:49:14 -0500

    add integration testing on app.test.ts for validate body not empty on req register account

--- src/test/integration/app.test.ts
+++ src/test/integration/app.test.ts
@@ -1,10 +1,17 @@
 import request from "supertest";
 import server from "../../server";
 
-describe("GET /", () => {
-  test("should return a welcome message", async () => {
-    const response = await request(server).get("/");
-    expect(response.status).toBe(200);
-    expect(response.text).toBe("Welcome to the Cash Tracker API");
+import { AuthController } from "../../controllers/AuthController";
+
+describe("Authentication - Register Account", () => {
+  test("should validation errors form is not empty", async () => {
+    const response = await request(server).post("/api/auth/register").send({});
+
+    const createAccountMock = jest.spyOn(AuthController, "register");
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(4);
+    expect(createAccountMock).not.toHaveBeenCalled();
   });
 });
