ce368c7f4faa616b4666542f8bab3dab0ccc4da3
HeymerDev <heymermeza11@gmail.com>
2026-05-08 22:26:22 -0500

    add integartion testing for validate password lenght

--- src/test/integration/app.test.ts
+++ src/test/integration/app.test.ts
@@ -30,4 +30,22 @@ describe("Authentication - Register Account", () => {
     expect(response.body.errors[0].msg).toBe("Invalid email address");
     expect(createAccountMock).not.toHaveBeenCalled();
   });
+
+  test("should validation password error if password is too short", async () => {
+    const response = await request(server).post("/api/auth/register").send({
+      name: "John Doe",
+      email: "john@example.com",
+      password: "short",
+    });
+
+    const createAccountMock = jest.spyOn(AuthController, "register");
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].msg).toBe(
+      "Password must be at least 8 characters long",
+    );
+    expect(createAccountMock).not.toHaveBeenCalled();
+  });
 });
