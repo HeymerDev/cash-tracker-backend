4c5c39ef39fe72ed501708e173062517df687a9e
HeymerDev <heymermeza11@gmail.com>
2026-05-21 21:33:40 -0500

    modified auth controller for regiter sharing token with test if enverioment is diferent to production

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -19,7 +19,13 @@ export class AuthController {
     try {
       const user = await User.create(req.body);
       user.password = await hashPassword(password);
-      user.token = generateToken();
+      const token = generateToken();
+      user.token = token;
+
+      if (process.env.NODE_ENV !== "production") {
+        globalThis.cashTrackerConfirmationToken = token;
+      }
+
       await user.save();
 
       await AuthEmail.sendVerificationEmail({
