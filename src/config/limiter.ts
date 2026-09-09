9a1db97487698813baafbd32f72f483171c0d1ec
HeymerDev <heymermeza11@gmail.com>
2026-09-08 23:18:18 -0500

    modified limiter.ts for add different rate limite on development

--- src/config/limiter.ts
+++ src/config/limiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  limit: process.env.NODE_ENV === "production" ? 5 : 5,
+  limit: process.env.NODE_ENV === "production" ? 5 : 20,
   message: {
     message: "Too many requests from this IP, please try again after a minute",
   },
